d35798a3579fe4f6a0e0f0073148460b0a1a2ae8
Fabian Fassbender <fabian.fassbender42@googlemail.com>
2014-01-01 18:12:18 +0100

    Fixed that a User gets multiple Join/Quit messages across channels

--- src/main/java/net/cubespace/CloudChat/Module/ChatHandler/Listener/PlayerQuitListener.java
+++ src/main/java/net/cubespace/CloudChat/Module/ChatHandler/Listener/PlayerQuitListener.java
@@ -4,13 +4,18 @@ import net.cubespace.CloudChat.CloudChatPlugin;
 import net.cubespace.CloudChat.Config.Main;
 import net.cubespace.CloudChat.Event.PlayerQuitEvent;
 import net.cubespace.CloudChat.Module.ChannelManager.ChannelManager;
-import net.cubespace.CloudChat.Module.ChatHandler.Event.ChatMessageEvent;
+import net.cubespace.CloudChat.Module.ChannelManager.Database.ChannelDatabase;
+import net.cubespace.CloudChat.Module.ChatHandler.Event.PlayerSendMessageEvent;
 import net.cubespace.CloudChat.Module.ChatHandler.Sender.Sender;
+import net.cubespace.CloudChat.Module.FormatHandler.Format.MessageFormat;
 import net.cubespace.CloudChat.Module.PlayerManager.Database.PlayerDatabase;
 import net.cubespace.CloudChat.Module.PlayerManager.PlayerManager;
 import net.cubespace.lib.EventBus.EventHandler;
 import net.cubespace.lib.EventBus.EventPriority;
 import net.cubespace.lib.EventBus.Listener;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+
+import java.util.ArrayList;
 
 /**
  * @author geNAZt (fabian.fassbender42@googlemail.com)
@@ -30,10 +35,21 @@ public class PlayerQuitListener implements Listener {
     @EventHandler(priority = EventPriority.NORMAL)
     public void onPlayerQuit(PlayerQuitEvent event) {
         PlayerDatabase playerDatabase = playerManager.get(event.getPlayer().getName());
+        ArrayList<ProxiedPlayer> sent = new ArrayList<>();
 
-        for(String channel : playerDatabase.JoinedChannels) {
-            Sender sender = new Sender(event.getPlayer().getName(), channelManager.get(channel), playerDatabase);
-            plugin.getAsyncEventBus().callEvent(new ChatMessageEvent(sender, ((Main) plugin.getConfigManager().getConfig("main")).Announce_PlayerQuitMessage));
+        for(ChannelDatabase channel : channelManager.getAllJoinedChannels(event.getPlayer())) {
+            ArrayList<ProxiedPlayer> inChannel = channelManager.getAllInChannel(channel);
+            String message = MessageFormat.format(((Main) plugin.getConfigManager().getConfig("main")).Announce_PlayerQuitMessage, channel, playerDatabase);
+            Sender sender = new Sender(event.getPlayer().getName(), channel, playerDatabase);
+
+            for(ProxiedPlayer player : inChannel) {
+                if(sent.contains(player)) continue;
+
+                plugin.getAsyncEventBus().callEvent(new PlayerSendMessageEvent(player, message, sender));
+                sent.add(player);
+            }
         }
+
+        sent.clear();
     }
 }
