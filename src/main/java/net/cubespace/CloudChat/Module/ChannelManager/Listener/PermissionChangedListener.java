dfa588edcd309c5a8224ef1bd5644106f99ccc83
Fabian Fassbender <fabian.fassbender42@googlemail.com>
2014-01-08 21:22:55 +0100

    Fixed a concurrent modification bug if Bukkit reloads the Permissions to fast

--- src/main/java/net/cubespace/CloudChat/Module/ChannelManager/Listener/PermissionChangedListener.java
+++ src/main/java/net/cubespace/CloudChat/Module/ChannelManager/Listener/PermissionChangedListener.java
@@ -1,22 +1,14 @@
 package net.cubespace.CloudChat.Module.ChannelManager.Listener;
 
 import net.cubespace.CloudChat.CloudChatPlugin;
-import net.cubespace.CloudChat.Config.Factions;
-import net.cubespace.CloudChat.Config.Main;
-import net.cubespace.CloudChat.Event.AsyncChatEvent;
 import net.cubespace.CloudChat.Module.ChannelManager.ChannelManager;
 import net.cubespace.CloudChat.Module.ChannelManager.Database.ChannelDatabase;
-import net.cubespace.CloudChat.Module.ChatHandler.Event.ChatMessageEvent;
-import net.cubespace.CloudChat.Module.ChatHandler.Sender.Sender;
-import net.cubespace.CloudChat.Module.PlayerManager.PlayerManager;
-import net.cubespace.CloudChat.Util.StringUtils;
 import net.cubespace.lib.EventBus.EventHandler;
 import net.cubespace.lib.EventBus.EventPriority;
 import net.cubespace.lib.Permission.Event.PermissionChangedEvent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 /**
  * @author geNAZt (fabian.fassbender42@googlemail.com)
@@ -25,12 +17,10 @@ import java.util.Arrays;
 public class PermissionChangedListener {
     private CloudChatPlugin plugin;
     private ChannelManager channelManager;
-    private PlayerManager playerManager;
 
     public PermissionChangedListener(CloudChatPlugin plugin) {
         this.plugin = plugin;
         this.channelManager = plugin.getManagerRegistry().getManager("channelManager");
-        this.playerManager = plugin.getManagerRegistry().getManager("playerManager");
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
@@ -42,7 +32,7 @@ public class PermissionChangedListener {
         //Check if Player still has permission for the Channels it is in
         ArrayList<ChannelDatabase> joinedChannels = channelManager.getAllJoinedChannels(player);
 
-        for(ChannelDatabase channelDatabase : joinedChannels) {
+        for(ChannelDatabase channelDatabase : new ArrayList<>(joinedChannels)) {
             if(!plugin.getPermissionManager().has(player, "cloudchat.channel." + channelDatabase.Name)) {
                 channelManager.leave(player, channelDatabase);
             }
