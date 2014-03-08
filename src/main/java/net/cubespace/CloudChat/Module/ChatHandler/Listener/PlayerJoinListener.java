ffa7805a9c0d6a84585df666782f9ed72b4c16c6
Fabian Fassbender <fabian.fassbender42@googlemail.com>
2014-03-08 18:06:36 +0100

    Fixed Player join Announce

--- src/main/java/net/cubespace/CloudChat/Module/ChatHandler/Listener/PlayerJoinListener.java
+++ src/main/java/net/cubespace/CloudChat/Module/ChatHandler/Listener/PlayerJoinListener.java
@@ -1,6 +1,7 @@
 package net.cubespace.CloudChat.Module.ChatHandler.Listener;
 
 import net.cubespace.CloudChat.Config.Messages;
+import net.cubespace.CloudChat.Event.PlayerJoinEvent;
 import net.cubespace.CloudChat.Module.ChannelManager.ChannelManager;
 import net.cubespace.CloudChat.Module.ChannelManager.Database.ChannelDatabase;
 import net.cubespace.CloudChat.Module.ChatHandler.Event.PlayerSendMessageEvent;
@@ -15,8 +16,6 @@ import net.cubespace.lib.CubespacePlugin;
 import net.cubespace.lib.EventBus.EventHandler;
 import net.cubespace.lib.EventBus.EventPriority;
 import net.cubespace.lib.EventBus.Listener;
-import net.cubespace.lib.Permission.Event.PermissionLoadedEvent;
-import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 import java.util.ArrayList;
@@ -24,36 +23,35 @@ import java.util.ArrayList;
 /**
  * @author geNAZt (fabian.fassbender42@googlemail.com)
  */
-public class PermissionLoadedListener implements Listener {
+public class PlayerJoinListener implements Listener {
     private CubespacePlugin plugin;
     private PlayerManager playerManager;
     private ChannelManager channelManager;
 
-    public PermissionLoadedListener(CubespacePlugin plugin) {
+    public PlayerJoinListener(CubespacePlugin plugin) {
         this.plugin = plugin;
         this.playerManager = plugin.getManagerRegistry().getManager("playerManager");
         this.channelManager = plugin.getManagerRegistry().getManager("channelManager");
     }
 
-    @EventHandler(priority = EventPriority.LOWEST)
-    public void onPlayerJoin(PermissionLoadedEvent event) {
-        ProxiedPlayer player = ProxyServer.getInstance().getPlayer(event.getPlayer());
-        if(plugin.getPermissionManager().has(player, "cloudchat.bypass.announce.join")) return;
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        if(plugin.getPermissionManager().has(event.getPlayer(), "cloudchat.bypass.announce.join")) return;
 
-        PlayerDatabase playerDatabase = playerManager.get(event.getPlayer());
+        PlayerDatabase playerDatabase = playerManager.get(event.getPlayer().getName());
         ArrayList<ProxiedPlayer> sent = new ArrayList<>();
 
-        for(ChannelDatabase channel : channelManager.getAllJoinedChannels(player)) {
+        for(ChannelDatabase channel : channelManager.getAllJoinedChannels(event.getPlayer())) {
             ArrayList<ProxiedPlayer> inChannel = channelManager.getAllInChannel(channel);
             String message = MessageFormat.format(((Messages) plugin.getConfigManager().getConfig("messages")).PlayerJoin, channel, playerDatabase);
-            Sender sender = new Sender(event.getPlayer(), channel, playerDatabase);
+            Sender sender = new Sender(event.getPlayer().getName(), channel, playerDatabase);
 
-            for(ProxiedPlayer playerIn : inChannel) {
-                if(sent.contains(playerIn)) continue;
+            for(ProxiedPlayer player : inChannel) {
+                if(sent.contains(player)) continue;
 
                 ClickEvent clickEvent = new ClickEvent();
                 clickEvent.setAction(ClickAction.RUN_COMMAND);
-                clickEvent.setValue("/cc:playermenu " + event.getPlayer());
+                clickEvent.setValue("/cc:playermenu " + event.getPlayer().getName());
 
                 ClickEvent clickEvent1 = new ClickEvent();
                 clickEvent1.setAction(ClickAction.RUN_COMMAND);
@@ -62,8 +60,8 @@ public class PermissionLoadedListener implements Listener {
                 MessageBuilder messageBuilder = new MessageBuilder();
                 messageBuilder.setText(message).addEvent("playerMenu", clickEvent).addEvent("focusChannel", clickEvent1);
 
-                plugin.getAsyncEventBus().callEvent(new PlayerSendMessageEvent(playerIn, messageBuilder, sender));
-                sent.add(playerIn);
+                plugin.getAsyncEventBus().callEvent(new PlayerSendMessageEvent(player, messageBuilder, sender));
+                sent.add(player);
             }
         }
 
