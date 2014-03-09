c616952c723b0d6c2594093ca1f21d8c8b6d8a60
Fabian Fassbender <fabian.fassbender42@googlemail.com>
2014-03-09 13:43:23 +0100

    Changed the PlayerMenu. You now can add custom Click Events to it. The Syntax for it is: {permission:<permission needed>; message:<message with [click] placeholder for the click place> ; command:<command with this playerholders: %player => the player you clicked on, %sender => your name>}

--- src/main/java/net/cubespace/CloudChat/Module/CloudChat/Command/Playermenu.java
+++ src/main/java/net/cubespace/CloudChat/Module/CloudChat/Command/Playermenu.java
@@ -42,72 +42,73 @@ public class Playermenu implements CLICommand {
         String message = messages.Playermenu_Complete;
         Integer amt = 0;
 
-        Matcher regexMatcher = pattern.matcher(message);
-        while (regexMatcher.find()) {
-            HashMap<String, String> found = new HashMap<>();
+        for(String line : message.split("\n")) {
+            Matcher regexMatcher = pattern.matcher(line);
+            while (regexMatcher.find()) {
+                HashMap<String, String> found = new HashMap<>();
 
-            String p = regexMatcher.group(1);
-            String[] temp = p.split(";");
+                String p = regexMatcher.group(1);
+                String[] temp = p.split(";");
 
-            for(String t : temp) {
-                String[] temp1 = t.split(":");
+                for(String t : temp) {
+                    String[] temp1 = t.split(":");
 
-                found.put(temp1[0].trim(), temp1[1]);
+                    found.put(temp1[0].trim(), temp1[1]);
+                }
+
+                if (found.containsKey("permission") && found.containsKey("command") && found.containsKey("message")) {
+                    if (!plugin.getPermissionManager().has(sender, found.get("permission"))) {
+                        plugin.getPluginLogger().debug("No Permission for " + regexMatcher.group(0));
+                        line = line.replace(regexMatcher.group(0), "");
+                        continue;
+                    }
+
+                    if (found.containsKey("status")) {
+                        switch (found.get("status")) {
+                            case "unmuted":
+                                if (muted) {
+                                    line = line.replace(regexMatcher.group(0), "");
+                                    plugin.getPluginLogger().debug("Wrong status for " + regexMatcher.group(0));
+                                    continue;
+                                }
+                                break;
+
+                            case "muted":
+                                if (!muted) {
+                                    line = line.replace(regexMatcher.group(0), "");
+                                    plugin.getPluginLogger().debug("Wrong status for " + regexMatcher.group(0));
+                                    continue;
+                                }
+                                break;
+                            default:
+                                line = line.replace(regexMatcher.group(0), "");
+                                plugin.getPluginLogger().debug("Wrong status for " + regexMatcher.group(0));
+                                continue;
+                        }
+                    }
+
+                    ClickAction clickAction = ClickAction.RUN_COMMAND;
+
+                    if (found.containsKey("mode")) {
+                        if (found.get("mode").trim().equals("suggest")) {
+                            clickAction = ClickAction.SUGGEST_COMMAND;
+                        }
+                    }
+
+                    ClickEvent clickEvent = new ClickEvent();
+                    clickEvent.setAction(clickAction);
+                    clickEvent.setValue(found.get("command").replace("%player", args[0]).replace("%sender", sender.getName()));
+
+                    messageBuilder.addEvent("c" + amt, clickEvent);
+                    line = line.replace(regexMatcher.group(0), found.get("message").replace("[click]", "{click:c" + amt +"}"));
+
+                    amt++;
+                } else {
+                    line = line.replace(regexMatcher.group(0), "");
+                }
             }
 
-            if (found.containsKey("permission") && found.containsKey("command") && found.containsKey("message")) {
-                if (!plugin.getPermissionManager().has(sender, found.get("permission"))) {
-                    System.out.println("No Permission for " + regexMatcher.group(0));
-                    message = message.replace(regexMatcher.group(0), "");
-                    continue;
-                }
-
-                if (found.containsKey("status")) {
-                    switch (found.get("status")) {
-                        case "unmuted":
-                            if (muted) {
-                                message = message.replace(regexMatcher.group(0), "");
-                                System.out.println("Wrong status for " + regexMatcher.group(0));
-                                continue;
-                            }
-                            break;
-
-                        case "muted":
-                            if (!muted) {
-                                message = message.replace(regexMatcher.group(0), "");
-                                System.out.println("Wrong status for " + regexMatcher.group(0));
-                                continue;
-                            }
-                            break;
-                        default:
-                            message = message.replace(regexMatcher.group(0), "");
-                            System.out.println("Wrong status for " + regexMatcher.group(0));
-                            continue;
-                    }
-                }
-
-                ClickAction clickAction = ClickAction.RUN_COMMAND;
-
-                if (found.containsKey("mode")) {
-                    if (found.get("mode").trim().equals("suggest")) {
-                        clickAction = ClickAction.SUGGEST_COMMAND;
-                    }
-                }
-
-                ClickEvent clickEvent = new ClickEvent();
-                clickEvent.setAction(clickAction);
-                clickEvent.setValue(found.get("command").replace("%player", args[0]).replace("%sender", sender.getName()));
-
-                messageBuilder.addEvent("c" + amt, clickEvent);
-                message = message.replace(regexMatcher.group(0), found.get("message").replace("[click]", "{click:c" + amt +"}"));
-
-                amt++;
-            } else {
-                message = message.replace(regexMatcher.group(0), "");
-            }
+            messageBuilder.setText(MessageFormat.format(line, null, playerManager.get(args[0]))).send(sender);
         }
-
-        System.out.println(message);
-        messageBuilder.setText(MessageFormat.format(message, null, playerManager.get(args[0]))).send(sender);
     }
 }
