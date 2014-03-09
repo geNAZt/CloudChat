25041c9815dcff82b9629bf6b3565a30228372a8
Fabian Fassbender <fabian.fassbender42@googlemail.com>
2014-03-09 16:06:06 +0100

    Fixed Stackoverflow Error in PermissionContainers

--- src/main/java/net/cubespace/CloudChat/Util/Permissions.java
+++ src/main/java/net/cubespace/CloudChat/Util/Permissions.java
@@ -6,6 +6,9 @@ import net.cubespace.lib.CubespacePlugin;
 import net.cubespace.lib.Permission.PermissionStorage;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author geNAZt (fabian.fassbender42@googlemail.com)
  */
@@ -17,18 +20,23 @@ public class Permissions {
     }
 
     public static void attachPermissionContainers(ProxiedPlayer proxiedPlayer) {
+        attachPermissionContainers(proxiedPlayer, new HashSet<String>());
+    }
+
+    private static void attachPermissionContainers(ProxiedPlayer proxiedPlayer, Set<String> alreadyAttached) {
         PermissionStorage permissionStorage = plugin.getPermissionManager().get(proxiedPlayer.getName());
         PermissionContainers permissionContainers = plugin.getConfigManager().getConfig("permissionContainers");
 
         for (PermissionContainer permission : permissionContainers.PermissionContainers) {
-            if (permissionStorage.has(permission.Access)) {
+            if (!alreadyAttached.contains(permission.Access) && permissionStorage.has(permission.Access)) {
                 for (String p : permission.Sub) {
                     if (!permissionStorage.has(p)) {
                         permissionStorage.add(p);
                     }
                 }
 
-                attachPermissionContainers(proxiedPlayer);
+                alreadyAttached.add(permission.Access);
+                attachPermissionContainers(proxiedPlayer, alreadyAttached);
             }
         }
     }
